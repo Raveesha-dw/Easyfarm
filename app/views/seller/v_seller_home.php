27fffb6d6d3910ebcd2c5326ab6a7bf2fd4b1c70
imalka9879 <imalkadhananja28@gmail.com>
2023-12-06 02:17:48 +0530

    seller home page is created

--- app/views/seller/v_seller_home.php
+++ app/views/seller/v_seller_home.php
@@ -3,14 +3,35 @@
 <?php require APPROOT . '/views/inc/components/sidebars/seller_sidebar.php'?>
  <div class ="msg-box">
         <div class ="m-header">
-                 <div class="m-img">
-                <img src="<?php echo URLROOT?>/public/images/seller/user.png" alt="">
-                </div>
-                <p>Imalka Dhananja wants to papaya<p>
-                <p> 2033 july 08</p>
+              <div class ="msg-ID">
+                     <div class="m-img">
+                            <img src="<?php echo URLROOT?>/public/images/seller/user.png" alt="">
+                     </div>
+                
+              </div>
+                <div class="msg-date"> <p>Imalka Dhananja</p>
+                      <p>2033 july 08<p>
+               </div>
                 
                 
         </div> 
+
+        <div class="msg-f"> 
+              
+                     <div class="msg-f-detail">
+                            <h3>Imalka Dhananja wants papaya in 2KG </h3>
+                     </div>
+                            <div class="msg-pdroduct">
+                                   <h3>Iteam            papaya</h3>
+                                   <h3>Amount           2KG</h3>
+                                   <h3>TO               Godagama,colombo</h3>
+                                   <h3>Method           Instopick up</h3>
+
+                            </div>
+                            <div class="button-msg">
+                                    <button class ="dd" >APPOROVE</button>
+                            </div>
+        </div>
         
  </div> 
 
