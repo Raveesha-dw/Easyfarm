9607f286b5f64696f42288d607fa33e329deeb74
imalka9879 <imalkadhananja28@gmail.com>
2023-12-06 12:28:24 +0530

    seller home css issues fixed

--- app/views/seller/v_seller_home.php
+++ app/views/seller/v_seller_home.php
@@ -2,38 +2,44 @@
 <?php require APPROOT . '/views/inc/components/navbars/home_nav.php'; ?>
 <?php require APPROOT . '/views/inc/components/sidebars/seller_sidebar.php'?>
  <div class ="msg-box">
-        <div class ="m-header">
+       <div class ="m-header">
               <div class ="msg-ID">
                      <div class="m-img">
                             <img src="<?php echo URLROOT?>/public/images/seller/user.png" alt="">
                      </div>
                 
               </div>
-                <div class="msg-date"> <p>Imalka Dhananja</p>
+              <div class="msg-date"> <p>Imalka Dhananja</p>
                       <p>2033 july 08<p>
-               </div>
+              </div>
                 
                 
-        </div> 
+       </div> 
 
-        <div class="msg-f"> 
+       <div class="msg-f"> 
               
                      <div class="msg-f-detail">
                             <h3>Imalka Dhananja wants papaya in 2KG </h3>
+                            <br>
                      </div>
                             <div class="msg-pdroduct">
-                                   <h3>Iteam            papaya</h3>
-                                   <h3>Amount           2KG</h3>
-                                   <h3>TO               Godagama,colombo</h3>
-                                   <h3>Method           Instopick up</h3>
+                            <h3>Iteam&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;papaya</h3>
+<h3>Amount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2KG</h3>
+<h3>TO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Godagama, Colombo</h3>
+<h3>Method&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Instopick up</h3>
+
 
                             </div>
                             <div class="button-msg">
                                     <button class ="dd" >APPOROVE</button>
                             </div>
-        </div>
+       </div>
         
  </div> 
+ <!-- <img src="<?php echo URLROOT?>/public/images/seller/user.png" alt=""> -->
+
+ <br></br>
+ 
 
 <?php require APPROOT . '/views/inc/footer.php';?>  
         
