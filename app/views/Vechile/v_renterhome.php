319ba129577517f2b5a4b7082fb67ec98ee82a46
imalka9879 <imalkadhananja28@gmail.com>
2023-12-12 01:35:04 +0530

    fixed css issues

--- app/views/Vechile/v_renterhome.php
+++ app/views/Vechile/v_renterhome.php
@@ -1,6 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <?php require APPROOT . '/views/inc/components/navbars/home_nav.php'; ?>
-<?php require APPROOT . '/views/inc/components/sidebars/re_sidebar.php'?>
+<?php require APPROOT . '/views/inc/components/sidebars/ve_sidebar.php'?>
 
 
 
