fc47a7a1cb4f4d8536b95b6c34d371cf3f8b8786
imalka9879 <imalkadhananja28@gmail.com>
2023-12-17 15:43:54 +0530

    myplan backend created

--- app/controllers/Plan.php
+++ app/controllers/Plan.php
@@ -12,9 +12,9 @@
     public function get_plan_details(){
         // print_r("d");
         
-        $data = $this->planModel->get_dataplan(('59'));
-        $data  = get_object_vars($data[0]);
-        $originalDate = $data['Date'];
+        $data = $this->planModel->get_dataplan(('1'));
+        $data1  = get_object_vars($data[0]);
+        $originalDate = $data1['Register_date'];
 
 // Create a DateTime object from the original date
         $dateTime = new DateTime($originalDate);
@@ -26,7 +26,43 @@
         $newDate = $dateTime->format('Y-m-d');
 
 // Update the array with the new date
-        $data['Date'] = $newDate;
-        $this->view('seller/v_plan', $data);
+        $data1['Date'] = $newDate;
+        $data2= $this->get_plan_details2();
+        $concatenatedData = array_merge($data1, $data2);
+        if ($concatenatedData['plan_id'] == 1) {
+                $concatenatedData['list_count'] = $concatenatedData[0]['listing_limit'] - $concatenatedData['list_count'];
+            } elseif ($concatenatedData['plan_id'] == 2) {
+                $concatenatedData['list_count'] = $concatenatedData[1]['listing_limit'] - $concatenatedData['list_count'];
+            } else {
+                // Assuming 'Unlimited' means a large number, you can use PHP_INT_MAX or any other large number
+                $concatenatedData['list_count'] = "Unlimited";
+            }
+        
+
+        print_r( $concatenatedData);
+        // $this->view('seller/v_plan', $data1);
+
     }
+
+    public function get_plan_details2(){
+        // print_r("d");
+        
+        $data = $this->planModel->get_dataplan1();
+        $result = [];
+
+        foreach ($data as $row) {
+                 $result[] = get_object_vars($row);
+        }
+        // $data2  = get_object_vars($data[1]);
+        return $result;
+       
+
+// Create a DateTime object from the original date
+       
+       
+
+
+        // $this->view('seller/v_plan', $data2);
+    }
+      
 }
--- app/models/M_plan.php
+++ app/models/M_plan.php
@@ -5,9 +5,21 @@ public function __construct(){
     $this->db=new Database();
 }
 
-public function get_dataplan($seller_ID){
-    $this->db->query("SELECT * FROM plan WHERE seller_ID = :seller_ID");
-    $this->db->bind(':seller_ID', $seller_ID);
+public function get_dataplan($plan_id,){
+    $this->db->query("SELECT * FROM reg_seller WHERE plan_id = :plan_id");
+    $this->db->bind(':plan_id', $plan_id);
+    $result=$this->db->resultSet();
+    // print_r($result);
+    return $result;
+    
+    // exit();
+
+
+}
+
+public function get_dataplan1(){
+    $this->db->query("SELECT * FROM plandetails");
+    // $this->db->bind(':seller_ID', $seller_ID);
     $result=$this->db->resultSet();
     // print_r($result);
     return $result;
