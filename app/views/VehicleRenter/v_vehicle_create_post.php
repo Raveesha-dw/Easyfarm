15cb0596060191f76135ba3ec13637a8c0b8da60
Hasintha <aashi.chandu5088@gmail.com>
2024-01-12 00:44:42 +0530

    Add the calander for creating post of the renting vehicle

--- app/views/VehicleRenter/v_vehicle_create_post.php
+++ app/views/VehicleRenter/v_vehicle_create_post.php
@@ -1,4 +1,4 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
+<?php require APPROOT . '/views/inc/header.php';?>
 <?php require APPROOT . '/views/inc/components/navbars/home_nav.php';?>
 <?php require APPROOT . '/views/inc/components/sidebars/vehicleRenter_sidebar.php'?>
 
@@ -12,11 +12,11 @@
 <!-- <?php print_r($data);?> -->
 <div class ="ss">
 <div class="Wrapper_Vehicle_Create_Post">
-    
-    <form class ="ddd" action="<?php echo URLROOT ?>/V_post/create_post" enctype="multipart/form-data" method="POST">
-                  
 
-   
+    <form class ="ddd" action="<?php echo URLROOT ?>/V_post/create_post" enctype="multipart/form-data" method="POST">
+
+
+
 
 
 
@@ -26,18 +26,18 @@
 
                      <!-- <div class="sdropdown1"> -->
                         <label for="Category"  class = "scdropdown1" name="Category"><b>Vehicle Category:</b></label>
-                        <!-- <span class="invalid"><?php if($data){ echo $data['Category_err'];} ?></span> -->
+                        <!-- <span class="invalid"><?php if ($data) {echo $data['Category_err'];}?></span> -->
                         <br>
                         <select name="V_category" id="sCategory" class="sCategory" >
 
                             <option disabled selected>Select Category</option>
-                            
+
                             <option value= "Vegatable" >Lorries $Trucks</option>
                             <option value= "Fruits" >Heavy Duty</option>
                             <option value= "Plants">Tractors</option>
                             <option value= "Seeds">Three Wheelers</option>
                         </select>
-                        
+
                     <!-- </div> -->
 
 
@@ -46,22 +46,22 @@
                         <label for="Item"><b>Owner Name</b></label>
                     <br>
                         <input id="sitem_name" name="V_name" type="textbox" placeholder="Enter the Vehicle Name" required value="" ><br/>
-                        <!-- <span class="invalid"><?php if($data){echo $data['Item_name_err'];} ?></span> -->
-                   
+                        <!-- <span class="invalid"><?php if ($data) {echo $data['Item_name_err'];}?></span> -->
+
                     </div>
 
                     <div class ="sitem">
                         <label for="Item"><b>Vehicle number</b></label>
                     <br>
                         <input id="sitem_name" name="V_number" type="textbox" placeholder="Enter the Vehicle number" required value="" ><br/>
-                        <!-- <span class="invalid"><?php if($data){echo $data['Item_name_err'];} ?></span> -->
+                        <!-- <span class="invalid"><?php if ($data) {echo $data['Item_name_err'];}?></span> -->
                     </div>
-                    
+
                     <div class ="sitem">
                         <label for="Item"><b>Contact number</b></label>
                     <br>
                         <input id="sitem_name" name="Contact_Number" type="textbox" placeholder="Enter the Contact number" required value="" ><br/>
-                        <!-- <span class="invalid"><?php if($data){echo $data['Item_name_err'];} ?></span> -->
+                        <!-- <span class="invalid"><?php if ($data) {echo $data['Item_name_err'];}?></span> -->
                     </div>
 
 
@@ -70,12 +70,12 @@
                         <label for ="stock"> <b>Rental Fee</b></label>
                         <br>
                         <input id="size" name="Rental_Fee" type="number" step="1" min = 0 placeholder="Rental Fee"  required value="">
-                        <!-- <span class="invalid"><?php if($data){echo $data['Unit_size_err'];}  ?></span> -->
+                        <!-- <span class="invalid"><?php if ($data) {echo $data['Unit_size_err'];}?></span> -->
                         </div>
                         <div class="sdropdown2">
                                 <label for="Category2"><b>Charging Unit:</b></label>
                                 <br>
-                                
+
                                     <select name="Charging_Unit" id="stype">
                                         <option disabled selected> </option>
                                         <option value="Kg">Per Hour</option>
@@ -84,12 +84,12 @@
                                         <option value="ml">Per month</option>
 
                                     </select>
-                                <!-- <span class="invalid"><?php if($data) {echo $data['Unit_type_err'];}  ?></span> -->
-                                
-                                
-                        </div> 
+                                <!-- <span class="invalid"><?php if ($data) {echo $data['Unit_type_err'];}?></span> -->
+
+
+                        </div>
                      </div>
-                
+
                 <br>
 
 
@@ -103,35 +103,35 @@
                     <input id="sAddress" name ="address" type="textbox" placeholder="Enter the Address Line3" >
                     <input id="sAddress" name ="address" type="textbox" placeholder="Enter the Address Line4" >
 
-                    
+
                 </div>
-                
+
                 <br>
                 <div>
-                
 
-                    
-                   
+
+
+
                 </div>
-                
+
                 <br>
                 <br>
-               
+
                 <br>
-               
+
                 <br>
                 <div class="sDescription">
                     <b>Descripition</b>
                     <br>
                     <input id="sdes" name="Description" type="text" placeholder="Enter Descripitiion" required value="">
-                    <!-- <span class="invalid"><?php if($data){echo $data['Description_err'];}  ?></span> -->
+                    <!-- <span class="invalid"><?php if ($data) {echo $data['Description_err'];}?></span> -->
                 </div>
-                
-            
-              <br><br>  
-        
+
+
+              <br><br>
+
             <div class="imgdelivery">
-              
+
             <div class="image">
                 <b>Upload image</b>
                     <br>
@@ -140,10 +140,44 @@
                     <input id="inside_imageq" name="Image" type="file" placeholder="Upload the Images">
                     <input id="inside_imageq" name="Image" type="file" placeholder="Upload the Images"> -->
                     <input id="inside_imageq" name="Image" type="file" placeholder="Upload the Images">
-                    <!-- <span class="invalid"><?php if($data){echo $data['Image_err'];}  ?></span> -->
+                    <!-- <span class="invalid"><?php if ($data) {echo $data['Image_err'];}?></span> -->
                 </div>
+<br>
+<br>
+
+         <div class="sdate">
+            
+<link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/components/fullcalendar.min.css">
+<script src="<?php echo URLROOT ?>\public\js\jquery.min.js"></script>
+<script src="<?php echo URLROOT ?>\public\js\moment.min.js"></script>
+<script src="<?php echo URLROOT ?>\public\js\fullcalendar.min.js"></script>
 
 
+
+<div class="wrapperCalendar">
+
+         <div class="cal" width=10000%>
+
+
+    <div class="response"></div>
+    <div id='calendar'></div>
+        </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+<script src="<?php echo URLROOT ?>\public\js\Vpost_calender.js"></script>
+
+
+
+        </div>
 
 
                 <br>
@@ -153,55 +187,38 @@
                 <div class="s1">
                     <button type="submit" id ="create"><b>create</b> </button>
                 </div>
-        
 
-            
 
-        
-        
+
+
+
+
     </form>
 
- 
- 
 
 
- 
- 
-         <div class="sdate">
 
 
-<div>
-  <p>Select dates</p>
-  <input type="date" id="date" />
-
-  <button>Add Date</button>
-</div>
-
-<div style="padding-top:20px;">
-  <select name="dates" id="dates" size="5">
-  </select>
-</div>
-
-<button id="finish">Finish</button>
-
-        </div>
- 
- 
- 
- 
- 
- 
- 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 </div>
 
 </div>
-<?php require APPROOT . '/views/inc/footer.php'; ?>  
+<?php require APPROOT . '/views/inc/footer.php';?>
 
 <!-- Your other head elements -->
 <scipt src="https://code.jquery.com/jquery-3.6.4.min.js"></scipt>
@@ -211,7 +228,7 @@
     document.querySelector("button").addEventListener("click", function() {
   const sel = document.querySelector("select");
   const d = document.querySelector("#date").value;
-  
+
   if (d === null || d === undefined)
   {
     alert("Select a date first.");
--- app/views/VehicleRenter/v_vehicle_createdpost.php
+++ app/views/VehicleRenter/v_vehicle_createdpost.php
@@ -1,88 +1,88 @@
 
-<!-- <?php require APPROOT . '/views/inc/csslinking.php'; ?> -->
-<?php require APPROOT . '/views/inc/header.php'; ?>
-<?php require APPROOT . '/views/inc/components/navbars/home_nav.php'; ?>
+<!-- <?php require APPROOT . '/views/inc/csslinking.php';?> -->
+<?php require APPROOT . '/views/inc/header.php';?>
+<?php require APPROOT . '/views/inc/components/navbars/home_nav.php';?>
 <?php require APPROOT . '/views/inc/components/sidebars/vehicleRenter_sidebar.php'?>
-<?php $products = json_decode(json_encode($data), true); ?>
+<?php $products = json_decode(json_encode($data), true);?>
 
 
 
 
         <div class="wrapperVcreatedPost">
 
-        
-        
-            
-            
-            
+
+
+
+
+
 
 
             <!--Posts-->
             <div class="product-container" id="product-vehicle-container">
-                <?php $products =$data; ?>
-                <?php foreach($products as $product) : ?>
-                  
+                <?php $products = $data;?>
+                <?php foreach ($products as $product): ?>
+
                     <div class="productt" id="product-vehicle">
 
-                    
-                        <img src="<?php echo URLROOT?>/public/images/vehicleRenter/<?php echo $product->Image;?> " alt="" class="poost1">
+
+                        <img src="<?php echo URLROOT ?>/public/images/vehicleRenter/<?php echo $product->Image; ?> " alt="" class="poost1">
                         <!-- <?php echo $product->Image; ?> -->
-                        
-                        
+
+
 
                         <!-- <div class="name"> --><br><br>
-                            <p><?php echo $product->V_category;?> For Rent - <?php echo $product->V_number;?></p>
-                            <p>LKR <?php echo $product->Rental_Fee;?></p>
-                            <!-- <b id="namee1"> <?php echo $product->V_Id;?> </b>  -->
-                             <!-- <b id="price1"> <?php echo  $product->Rental_Fee;?> </b>  -->
+                            <p><?php echo $product->V_category; ?> For Rent - <?php echo $product->V_number; ?></p>
+                            <p>LKR <?php echo $product->Rental_Fee; ?></p>
+                            <!-- <b id="namee1"> <?php echo $product->V_Id; ?> </b>  -->
+                             <!-- <b id="price1"> <?php echo $product->Rental_Fee; ?> </b>  -->
 
-                            
+
                         <!-- </div> -->
-                        
-                        
 
-                        
+
+
+
                             <div class="button-n">
-                                <input  type="hidden" name="Item_Id" value="<?php echo $product->Item_Id;?>">
+                                <input  type="hidden" name="Item_Id" value="<?php echo $product->Item_Id; ?>">
 
-                                    <button class ="btn2"><a   href="http://localhost/Easyfarm/Seller_post/update_Product?id=<?php echo $product->Item_Id;?>">update</a></button>
+                                    <button class ="btn2"><a   href="http://localhost/Easyfarm/Seller_post/update_Product?id=<?php echo $product->Item_Id; ?>">update</a></button>
                                     <!-- <p>Update  </p>
                                     </a> -->
                                 <form  onsubmit="showRemoveConfirmation( );" method="post" action="<?php echo URLROOT ?>/Seller_post/delete_product">
-                                    <input type="hidden" name="Item_Id" value="<?php echo $product->Item_Id;?>">
+                                    <input type="hidden" name="Item_Id" value="<?php echo $product->Item_Id; ?>">
                                     <button  type="submit" class="buttonn" id="btnv3" name="delete_item">Delete</button>
                                 </form>
                             </div>
-                        
-                    </div>
-                <?php endforeach ?>  
-            </div>
-        </div>   
-            
-    
 
-        
-        
+                    </div>
+                <?php endforeach?>
+            </div>
+        </div>
+
+
+
+
+
 <script>
     // let subMenu = document.getElementById("subMenu");
     // function toggleMenu(){
     //     subMenu.classList.toggle("open-menu");
     // }
-   
 
-    
+
+
 
 
     // Function to show the remove confirmation popup
     function showRemoveConfirmation() {
-        return confirm('Are you sure you want to delete this item?'); 
+        return confirm('Are you sure you want to delete this item?');
             // Handle item removal here, e.g., by making an AJAX request
-                    
+
             }
 
-        
+
 
 </script>
-<?php require APPROOT . '/views/inc/footer.php'; ?>   
+<?php require APPROOT . '/views/inc/footer.php';?>
 
 
