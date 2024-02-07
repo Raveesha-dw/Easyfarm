acd7e078ad08c4c5f32caadc2e8a31423227e89a
Imalkaaa <imalkadhananja28@gmail.com>
2024-02-07 18:16:18 +0530

    caladenr some part ccreated

--- public/js/Vpost_calender1.js
+++ public/js/Vpost_calender1.js
@@ -1,3 +1,188 @@
+// $(document).ready(function () {
+//     var addedDates = {};
+
+//     initializeCalendar();
+
+//     function initializeCalendar() {
+//         addedDates = {};
+
+//         var calendar = $('#calendar').fullCalendar({
+//             editable: true,
+//             events: "../Calendar/fetch_anavailble_Dates",
+//             displayEventTime: false,
+//             selectable: true,
+//             selectHelper: true,
+//             select: function (start, end, allDay) {
+//                 var selectedDate = start.format('YYYY-MM-DD');
+
+//                 if (!addedDates[selectedDate]) {
+//                     var title = "ADDED";
+//                     if (title) {
+//                         $.ajax({
+//                             url: '../V_post/create_post',
+//                             data: 'title=' + title + '&start=' + selectedDate + '&end=' + selectedDate,
+//                             type: "POST",
+//                             success: function (data) {
+//                                 displayMessage("Added Successfully");
+//                                 addedDates[selectedDate] = true;
+//                                 calendar.fullCalendar('renderEvent', {
+//                                     title: title,
+//                                     start: selectedDate,
+//                                     end: selectedDate,
+//                                     allDay: true
+//                                 }, true);
+//                             }
+//                         });
+//                     }
+//                 } else {
+//                     alert("An event has already been added for this date.");
+//                 }
+//                 calendar.fullCalendar('unselect');
+//             }
+//         });
+
+
+//         function highlightAddedDate(date) {
+//             var $dateCell = $('.fc-day[data-date="' + date + '"]');
+//             $dateCell.css('background-color', 'black');
+//         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         // Prevent dragging for multiple date selection
+//         var isDragging = false;
+//         var dragThreshold = 5; // Adjust this value if needed for sensitivity
+
+//         $('#calendar').on('mousedown', function (e) {
+//             var startX = e.pageX,
+//                 startY = e.pageY;
+
+//             $(document).on('mousemove.drag', function (e) {
+//                 var currentX = e.pageX,
+//                     currentY = e.pageY;
+
+//                 if (Math.abs(currentX - startX) > dragThreshold || Math.abs(currentY - startY) > dragThreshold) {
+//                     isDragging = true;
+//                     $(document).off('mousemove.drag');
+//                 }
+//             });
+//         });
+
+//         $(document).on('mouseup', function () {
+//             $(document).off('mousemove.drag');
+//             if (isDragging) {
+//                 isDragging = false;
+//                 return false; // Prevent further action
+//             }
+//         });
+//     }
+
+//     function displayMessage(message) {
+//         $(".response").html("<div class='success'>" + message + "</div>");
+//         setInterval(function () { $(".success").fadeOut(); }, 2000);
+//     }
+// });
+
+
+
+// $(document).ready(function () {
+//     var addedDates = {};
+
+//     initializeCalendar();
+
+//     function initializeCalendar() {
+//         addedDates = {};
+
+//         var calendar = $('#calendar').fullCalendar({
+//             editable: true,
+//             events: "../Calendar/fetch_anavailble_Dates",
+//             displayEventTime: false,
+//             selectable: true,
+//             selectHelper: true,
+//             select: function (start, end, allDay) {
+//                 var selectedDate = start.format('YYYY-MM-DD');
+
+//                 if (!addedDates[selectedDate]) {
+//                     var title = "ADDED";
+//                     if (title) {
+//                         $.ajax({
+//                             url: '../V_post/create_post',
+//                             data: 'title=' + title + '&start=' + selectedDate + '&end=' + selectedDate,
+//                             type: "POST",
+//                             success: function (data) {
+//                                 displayMessage("Added Successfully");
+//                                 addedDates[selectedDate] = true;
+//                                 highlightAddedDate(selectedDate);
+//                             }
+//                         });
+//                     }
+//                 } else {
+//                     alert("An event has already been added for this date.");
+//                 }
+//                 calendar.fullCalendar('unselect');
+//             }
+//         });
+
+//         // Prevent dragging for multiple date selection
+//         var isDragging = false;
+//         var dragThreshold = 5; // Adjust this value if needed for sensitivity
+
+//         $('#calendar').on('mousedown', function (e) {
+//             var startX = e.pageX,
+//                 startY = e.pageY;
+
+//             $(document).on('mousemove.drag', function (e) {
+//                 var currentX = e.pageX,
+//                     currentY = e.pageY;
+
+//                 if (Math.abs(currentX - startX) > dragThreshold || Math.abs(currentY - startY) > dragThreshold) {
+//                     isDragging = true;
+//                     $(document).off('mousemove.drag');
+//                 }
+//             });
+//         });
+
+//         $(document).on('mouseup', function () {
+//             $(document).off('mousemove.drag');
+//             if (isDragging) {
+//                 isDragging = false;
+//                 return false; // Prevent further action
+//             }
+//         });
+//     }
+
+//     function highlightAddedDate(date) {
+//         var $dateCell = $('.fc-day[data-date="' + date + '"]');
+//         $dateCell.css('background-color', 'white');
+//         $dateCell.append('<span class="added-text">ADDED</span>'); // Append the text "ADDED"
+//     }
+    
+
+//     function displayMessage(message) {
+//         $(".response").html("<div class='success'>" + message + "</div>");
+//         setInterval(function () { $(".success").fadeOut(); }, 2000);
+//     }
+// });
+
+
+
 $(document).ready(function () {
     var addedDates = {};
 
@@ -6,76 +191,77 @@ $(document).ready(function () {
     function initializeCalendar() {
         addedDates = {};
 
+        var today = moment().startOf('day'); // Get today's date
         var calendar = $('#calendar').fullCalendar({
             editable: true,
             events: "../Calendar/fetch_anavailble_Dates",
             displayEventTime: false,
             selectable: true,
             selectHelper: true,
-            select: function (start, end, allDay) {
-                var selectedDate = $.fullCalendar.formatDate(start, "Y-MM-DD");
-                
+            validRange: {
+                start: today // Set the valid range to start from today
+            },
+            select: function (start) {
+                var selectedDate = start.format('YYYY-MM-DD');
+
                 if (!addedDates[selectedDate]) {
                     var title = "ADDED";
                     if (title) {
-                        var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                        var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-
                         $.ajax({
                             url: '../V_post/create_post',
-                            data: 'title=' + title + '&start=' + start + '&end=' + end,
+                            data: 'title=' + title + '&start=' + selectedDate + '&end=' + selectedDate,
                             type: "POST",
                             success: function (data) {
                                 displayMessage("Added Successfully");
                                 addedDates[selectedDate] = true;
+                                highlightAddedDate(selectedDate);
                             }
                         });
-
-                        calendar.fullCalendar('renderEvent', {
-                            title: title,
-                            start: start,
-                            end: end,
-                            allDay: allDay
-                        }, true);
                     }
-                    calendar.fullCalendar('unselect');
                 } else {
                     alert("An event has already been added for this date.");
                 }
-            },
-            eventDrop: function (event, delta) {
-                var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                // $.ajax({
-                //     url: '../Calendar/edit_anavailble_Dates',
-                //     data: 'title=' + event.title + '&start=' + start + '&end=' + end + '&id=' + event._id,
-                //     type: "POST",
-                //     success: function (response) {
-                //         displayMessage("Updated Successfully");
-                //     }
-                // });
-            },
-            eventClick: function (event) {
-                var deleteMsg = confirm("Do you really want to delete?");
-                if (deleteMsg) {
-                    // $.ajax({
-                    //     type: "POST",
-                    //     url: "../Calendar/delete_anavailble_Dates",
-                    //     data: "id=" + event.id,
-                    //     success: function (response) {
-                    //         if (parseInt(response) > 0) {
-                    //             $('#calendar').fullCalendar('removeEvents', event.id);
-                    //             displayMessage("Deleted Successfully");
-                    //             var selectedDate = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
-                    //             addedDates[selectedDate] = false;
-                    //         }
-                    //     }
-                    // });
+                calendar.fullCalendar('unselect');
+            }
+        });
+
+        // Prevent dragging for multiple date selection
+        var isDragging = false;
+        var dragThreshold = 5; // Adjust this value if needed for sensitivity
+
+        $('#calendar').on('mousedown', function (e) {
+            var startX = e.pageX,
+                startY = e.pageY;
+
+            $(document).on('mousemove.drag', function (e) {
+                var currentX = e.pageX,
+                    currentY = e.pageY;
+
+                if (Math.abs(currentX - startX) > dragThreshold || Math.abs(currentY - startY) > dragThreshold) {
+                    isDragging = true;
+                    $(document).off('mousemove.drag');
                 }
+            });
+        });
+
+        $(document).on('mouseup', function () {
+            $(document).off('mousemove.drag');
+            if (isDragging) {
+                isDragging = false;
+                return false; // Prevent further action
             }
         });
     }
 
+    function highlightAddedDate(date) {
+        var $dateCell = $('.fc-day[data-date="' + date + '"]');
+        $dateCell.css({
+            'background-color': 'black',
+            'position': 'relative' // Set position to relative to position the added text
+        });
+        $dateCell.append('<span class="added-text">ADDED</span>'); // Append the text "ADDED" inside the date square
+    }
+    
     function displayMessage(message) {
         $(".response").html("<div class='success'>" + message + "</div>");
         setInterval(function () { $(".success").fadeOut(); }, 2000);
