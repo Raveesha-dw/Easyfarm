fde82f8947d6bb48203e63eec5087aec2dfafc68
Anjana Tharusha <anjanatharusha13@gmail.com>
2024-02-10 13:56:35 +0530

    Add sidebar and CSS to admin dashboard

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -8,6 +8,10 @@ class Admin extends Controller{
     }
 
     public function index(){
+        
+    }
+
+    public function blog(){
         $data['blogCategories'] = $this->adminModel->getCategories();
 
         $newCategory = [
@@ -20,7 +24,7 @@ class Admin extends Controller{
 
         $data['newCategory'] = $newCategory;
         
-        $this->view('Admin/v_adminDashboard', $data);
+        $this->view('Admin/v_adminBlog', $data);
     }
 
     public function addcategory(){
@@ -55,7 +59,44 @@ class Admin extends Controller{
                 }
             }else{
                 $data['newCategory'] = $newcategory;
-                $this->view('Admin/v_adminDashboard', $data);
+                $this->view('Admin/v_adminBlog', $data);
+            }
+        }
+    }
+
+    public function editcategory(){
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_UNSAFE_RAW);
+
+            $newcategory = [
+                'category' => trim($_POST['category']),
+                'permalink' => trim($_POST['permalink']),
+
+                'category_err' => '',
+                'permalink_err' => ''
+            ];
+
+            if(empty($newcategory['category'])){
+                $newcategory['category_err'] = 'Enter category name';
+            }
+
+            if(empty($newcategory['permalink'])){
+                $newcategory['permalink_err'] = 'Permalink should not be empty';
+            }
+            else if($this->adminModel->findCategory($newcategory['permalink'])){
+                $newcategory['permalink_err'] = 'This permalink is already used';
+            }
+            
+            if(empty($newcategory['category_err']) && empty($newcategory['permalink_err'])){
+                if($this->adminModel->insertCategory($newcategory)){
+                    flash('add_category_success', 'Category has been added successfully!');
+                    header('Location: ' . URLROOT . '/Admin');
+                }else{
+                    die('Something went wrong :(');
+                }
+            }else{
+                $data['newCategory'] = $newcategory;
+                $this->view('Admin/v_adminBlog', $data);
             }
         }
     }
