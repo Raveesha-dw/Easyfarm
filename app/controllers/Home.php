a72285da3e507ca3ceeb906febf96e93ab6cf5ca
Hasintha <aashi.chandu5088@gmail.com>
2024-02-21 12:48:42 +0530

    fix the code

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -12,15 +12,15 @@ class Home extends Controller{
         
     }
 
-public function get_product_details(){
+// public function get_product_details(){
 
-    $data=$this->HomeModel->get_data();
+//     $data=$this->HomeModel->get_data();
     
-    $this->view('pages/home',$data);
+//     $this->view('pages/home',$data);
     
     
 
-}
+// }
 
 
 
--- app/models/M_home.php
+++ app/models/M_home.php
@@ -5,11 +5,11 @@ public function __construct(){
     $this->db=new Database();
 }
 
-public function get_data(){
-    $this->db->query("SELECT * FROM item ");
-    $result=$this->db->resultSet();
-    print_r($result);
-    return $result;
-}
+// public function get_data(){
+//     $this->db->query("SELECT * FROM item ");
+//     $result=$this->db->resultSet();
+//     print_r($result);
+//     return $result;
+// }
 }
 
