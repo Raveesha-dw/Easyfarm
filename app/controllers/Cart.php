da77f923838f5b5816b9068e033dd08c106c1e0a
Hasintha <aashi.chandu5088@gmail.com>
2024-02-27 21:52:32 +0530

    Change Cart.php

--- app/controllers/Cart.php
+++ app/controllers/Cart.php
@@ -9,27 +9,26 @@ class Cart extends Controller{
     public function __construct()
     {
         $this->cartModel = $this->model('M_cart');
-        
+
     }
 
-
-    public function showCart() {
+    public function showCart()
+    {
 
         $data = [
-            'quantity' => '', 
-            'itemId' => '', 
-            'uId' => $_SESSION['user_ID'], 
-            
+            'quantity' => '',
+            'itemId' => '',
+            'uId' => $_SESSION['user_ID'],
+
         ];
 
-
-        $Mitems =$this->cartModel->getAllcartItems($data);
+        $Mitems = $this->cartModel->getAllcartItems($data);
         $items = (array) $Mitems;
         $data = array();
         foreach ($items as $Mitem) {
             $viewItem = array();
             $item = (array) $Mitem;
-            $viewItem['quantity'] = $item ['Quantity'];
+            $viewItem['quantity'] = $item['Quantity'];
             $viewItem['itemId'] = $item['Item_Id'];
             $viewItem['uId'] = $item['U_Id'];
             $viewItem['unitPrice'] = $item['Unit_price'];
@@ -45,111 +44,83 @@ class Cart extends Controller{
 
     }
 
+    public function addToCart()
+    {
 
-    public function addToCart() {
-
-        
-        if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_SESSION['user_ID'])  && ($_SESSION['user_type'] == 'Buyer'))) {
+        if (($_SERVER['REQUEST_METHOD'] == 'POST') && (!empty($_SESSION['user_ID']) && ($_SESSION['user_type'] == 'Buyer'))) {
             $_POST = filter_input_array(INPUT_POST, FILTER_UNSAFE_RAW);
 
-            if((!empty($_POST['quantity'])) && (!empty($_POST['delivery'])) ){
+            if ((!empty($_POST['quantity'])) && (!empty($_POST['delivery']))) {
 
                 $data = [
-                    'quantity' => $_POST['quantity'], 
-                    'itemId' => $_POST['itemId'], 
+                    'quantity' => $_POST['quantity'],
+                    'itemId' => $_POST['itemId'],
                     'uId' => $_POST['uId'],
                     'selectedDeliveryMethod' => $_POST['delivery'],
    
-    
-                    
+   
+       
                 ];
     
-                
-                // error_log(print_r($data, TRUE)); 
     
+                // error_log(print_r($data, TRUE));
+
                 // Check if the cart item already exists for this combination of itemId and uId
-                if ($existingCartItem = $this->cartModel->getCartItem($data)) {            
+                if ($existingCartItem = $this->cartModel->getCartItem($data)) {
                     // If the cart item already exists, update the quantity
                     // $data['quantity'] += $existingCartItem->Quantity;
                     // $data['quantity'] += intval($existingCartItem->Quantity);
                     $data['quantity'] = intval($data['quantity']) + intval($existingCartItem->Quantity);
-    
-    
-                    if ($this->cartModel->updateCartItem(['uId'=>$existingCartItem->U_Id,'quantity' => $data['quantity'] , 'itemId' => $data['itemId'] , 'selectedDeliveryMethod' => $data['selectedDeliveryMethod']])) {
-                        // $this->view('pages/cart', $data);                  
+
+                    if ($this->cartModel->updateCartItem(['uId' => $existingCartItem->U_Id, 'quantity' => $data['quantity'], 'itemId' => $data['itemId'], 'selectedDeliveryMethod' => $data['selectedDeliveryMethod']])) {
+                        // $this->view('pages/cart', $data);
                     } else {
                         die('Something went wrong while updating the cart item.');
                     }
-                } 
-                else 
-                {
+                } else {
                     $this->cartModel->addToCart($data);
-          
+
                 }
-    
-                $Mitems =$this->cartModel->getAllcartItems($data);
-                $items = (array) $Mitems;
-                $data = array();
-                foreach ($items as $Mitem) {
-                    $viewItem = array();
-                    $item = (array) $Mitem;
-                    $viewItem['quantity'] = $item ['Quantity'];
-                    $viewItem['itemId'] = $item['Item_Id'];
-                    $viewItem['uId'] = $item['U_Id'];
-                    $viewItem['unitPrice'] = $item['Unit_price'];
-                    $viewItem['itemName'] = $item['Item_name'];
-                    $viewItem['selectedDeliveryMethod'] = $item['selectedDeliveryMethod'];
-                    $viewItem['Unit_type'] = $item['Unit_type'];
-                    $viewItem['Unit_size'] = $item['Unit_size'];
-                    array_push($data, $viewItem);
-    
-    
-                }
-                // $mergedArray = array_merge($data1, $data);
-                // $data = array_unique($mergedArray);
-                // $this->view('pages/cart', $data);
+
                 redirect('Cart/getCart');
             }
 
+        } elseif (!empty($_SESSION['user_ID']) && ($_SESSION['user_type'] != 'Buyer')) {
 
-            
-        }elseif(!empty($_SESSION['user_ID']) && ($_SESSION['user_type'] != 'Buyer')  ){
-
-            $data=[
-                'user_type'=> '',
-                'fullname'=>'',
-                'contactno'=>'',
+            $data = [
+                'user_type' => '',
+                'fullname' => '',
+                'contactno' => '',
                 'email' => '',
                 'address' => '',
                 'city' => '',
                 'postalcode' => '',
-                'password'=>'',
-                'confirm-password'=>'',
+                'password' => '',
+                'confirm-password' => '',
 
                 'name_err' => '',
                 'contactno_err' => '',
                 'email_err' => '',
                 'address_err' => '',
-                'password_err'=>'',
-                'confirm-password_err'=>'',
+                'password_err' => '',
+                'confirm-password_err' => '',
 
             ];
-            $this->view('Users/v_registerBuyer',$data);
+            $this->view('Users/v_registerBuyer', $data);
 
-        }
-        elseif(empty($_SESSION['user_ID'])  ){  
-            $data=[
+        } elseif (empty($_SESSION['user_ID'])) {
+            $data = [
                 'email' => '',
                 'password' => '',
 
                 'email_err' => '',
-                'password_err' => ''
+                'password_err' => '',
             ];
             $this->view('Users/v_login', $data);
 
             // redirect('Users/v_login');
 
-        }else {
+        } else {
             // redirect('pages/cart');
             $data = [
                 'unitPrice' => '',
@@ -159,51 +130,49 @@ class Cart extends Controller{
             ];
             $this->view('pages/cart', $data);
         }
-    
+
     }
 
-public function getCart(){
+    public function getCart()
+    {
 
-    
-                $Mitems =$this->cartModel->getAllcartItems1($_SESSION['user_ID']);
-                $items = (array) $Mitems;
-                $data = array();
-                foreach ($items as $Mitem) {
-                    $viewItem = array();
-                    $item = (array) $Mitem;
-                    $viewItem['quantity'] = $item ['Quantity'];
-                    $viewItem['itemId'] = $item['Item_Id'];
-                    $viewItem['uId'] = $item['U_Id'];
-                    $viewItem['unitPrice'] = $item['Unit_price'];
-                    $viewItem['itemName'] = $item['Item_name'];
-                    $viewItem['selectedDeliveryMethod'] = $item['selectedDeliveryMethod'];
-                    $viewItem['Unit_type'] = $item['Unit_type'];
-                    $viewItem['Unit_size'] = $item['Unit_size'];
-                    array_push($data, $viewItem);
-    
-    
-                }
-                // $mergedArray = array_merge($data1, $data);
-                // $data = array_unique($mergedArray);
-                $this->view('pages/cart', $data);
+        $Mitems = $this->cartModel->getAllcartItems1($_SESSION['user_ID']);
+        $items = (array) $Mitems;
+        $data = array();
+        foreach ($items as $Mitem) {
+            $viewItem = array();
+            $item = (array) $Mitem;
+            $viewItem['quantity'] = $item['Quantity'];
+            $viewItem['itemId'] = $item['Item_Id'];
+            $viewItem['uId'] = $item['U_Id'];
+            $viewItem['unitPrice'] = $item['Unit_price'];
+            $viewItem['itemName'] = $item['Item_name'];
+            $viewItem['selectedDeliveryMethod'] = $item['selectedDeliveryMethod'];
+            $viewItem['Unit_type'] = $item['Unit_type'];
+            $viewItem['Unit_size'] = $item['Unit_size'];
+            array_push($data, $viewItem);
 
-}
+        }
+        // $mergedArray = array_merge($data1, $data);
+        // $data = array_unique($mergedArray);
+        $this->view('pages/cart', $data);
 
+    }
 
-
-    public function deleteItem() {
+    public function deleteItem()
+    {
 
         // $data = $this->cartModel->getCartItem($_GET);
 
         if ($this->cartModel->deleteCartItem($_GET)) {
 
-            $Mitems =$this->cartModel->getAllcartItems($_GET);
+            $Mitems = $this->cartModel->getAllcartItems($_GET);
             $items = (array) $Mitems;
             $data = array();
             foreach ($items as $Mitem) {
                 $viewItem = array();
                 $item = (array) $Mitem;
-                $viewItem['quantity'] = $item ['Quantity'];
+                $viewItem['quantity'] = $item['Quantity'];
                 $viewItem['itemId'] = $item['Item_Id'];
                 $viewItem['uId'] = $item['U_Id'];
                 $viewItem['unitPrice'] = $item['Unit_price'];
@@ -215,30 +184,13 @@ public function getCart(){
                 array_push($data, $viewItem);
             }
 
-
             $this->view('pages/cart', $data);
 
-
         } else {
-            
+
             die('Failed to delete the item from the cart.');
         }
 
     }
 
-
-
-        
-
-
-
-   
 }
-
-
-
-
-
-
-
-?>
