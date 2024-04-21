14060512b01b105d0c496ffbfdc0784d4bec8d62
Imalkaaa <imalkadhananja28@gmail.com>
2024-04-21 11:02:14 +0530

    change seller home nav

--- app/views/inc/components/navbars/renter_nav.php
+++ app/views/inc/components/navbars/renter_nav.php
@@ -1,5 +1,40 @@
 <div class="homeheader">
-    <a href="http://localhost/Easyfarm/V_renter_home/get_details1">NEW ORDERS</a>
-    <a href="http://localhost/Easyfarm/V_renter_home/get_details2">PENDING</a>
-    <a href="http://localhost/Easyfarm/V_renter_home/get_details3">COMPLETED</a>
+    <a href="http://localhost/Easyfarm/V_renter_home/get_details1" class="link" >NEW ORDERS</a>
+    <a href="http://localhost/Easyfarm/V_renter_home/get_details2" class="link" >PENDING</a>
+    <a href="http://localhost/Easyfarm/V_renter_home/get_details3" class="link" >COMPLETED</a>
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function(){
+    // Function to set active link and color
+    function setActiveLink(link) {
+        $('.link').removeClass('active');
+        $(link).addClass('active');
+        localStorage.setItem('activeLink', $(link).attr('href'));
+    }
+    
+    // Set active link based on the current URL
+    var currentURL = window.location.href;
+    $('.link').each(function() {
+        if ($(this).attr('href') === currentURL) {
+            setActiveLink(this);
+            return false; // exit the loop once the active link is set
+        }
+    });
+    
+    // Click event handler for links
+    $('.link').click(function(event){
+        event.preventDefault();
+        setActiveLink(this);
+        window.location.href = $(this).attr('href');
+    });
+});
+</script>
+
+<style>
+.active {
+    color: red; /* Change 'red' to the desired color */
+}
+</style>
