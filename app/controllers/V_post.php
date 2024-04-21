c8f6faed51a7562ee5912eef912d0c7b7849421d
Hasintha <aashi.chandu5088@gmail.com>
2024-04-21 14:51:19 +0530

    update the view more details booking pop up message

--- app/controllers/V_post.php
+++ app/controllers/V_post.php
@@ -392,7 +392,7 @@ else{
 
                 if ($booking_date->status == 'Pending') {
                     $unconfirmed_booking_dates[] = $booking_date->date;
-                } elseif ($booking_date->status == 'success') {
+                } elseif ($booking_date->status == 'Accept') {
                     $confirmed_booking_dates[] = $booking_date->date;
                 }
 
