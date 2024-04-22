2625e16295b69824e4dca57094b91048fd98bb8a
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2024-04-22 10:45:58 +0530

    changes to models

--- app/models/M_buyNow.php
+++ app/models/M_buyNow.php
@@ -24,7 +24,7 @@ class M_buyNow{
     public function getItemDetais($data){
 
         // $this->db->query('SELECT Item_name,DeliveryMethod,Unit_price,Unit_type,Unit_size  FROM item WHERE item.Item_Id = :Item_Id') ;
-        $this->db->query('SELECT I.Item_name, I.DeliveryMethod, I.Unit_price, I.Unit_type, I.Unit_size, S.U_Id as Store_seller, S.Store_Name FROM item I INNER JOIN reg_seller S ON I.seller_ID=S.U_Id WHERE I.Item_Id = :Item_Id');
+        $this->db->query('SELECT I.Item_name, I.DeliveryMethod, I.Unit_price, I.Unit_type, I.Unit_size, I.Image, S.U_Id as Store_seller, S.Store_Name FROM item I INNER JOIN reg_seller S ON I.seller_ID=S.U_Id WHERE I.Item_Id = :Item_Id');
         $this->db->bind(':Item_Id', $data['Item_Id']);
         $this->db->execute();
             
