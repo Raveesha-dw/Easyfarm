08d555d54653b6990e2aa1c4ffdce59666e535ad
Hasintha <aashi.chandu5088@gmail.com>
2024-04-24 21:31:47 +0530

    update the home page

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -2,15 +2,13 @@
 <?php require APPROOT . '/views/inc/components/navbars/home_nav.php'; ?>
  
 
-<?php print_r($_SESSION['a'] ) ?>
+<!-- <?php print_r($data ) ?> -->
 <div class="wrapper">
     <!--Menu-->
     <section class="menu">
         <div class="container">
             <h2 class="title">Featured Categories</h2>
-            <?php print_r($_SESSION['a'] );print_r("eeeeee"); ?>
-            <?php print_r($_SESSION['b'] ) ?>
-            <?php print_r($_SESSION['c'] ) ?>
+       
             <div class="menu-container">
                 <div class="card">
                     <i class="fas fa-carrot"></i>
