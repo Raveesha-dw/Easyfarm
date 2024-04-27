95e1aa4fba17e87db89c49b06f7e3bd1cbf756f0
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2024-04-27 12:54:53 +0530

    order controller changes

--- app/models/M_orders.php
+++ app/models/M_orders.php
@@ -52,6 +52,14 @@ class M_orders{
 
     }
 
+    public function getAcceptOrders(){
+        $this->db->query('SELECT * FROM orders INNER JOIN order_details on orders.Order_ID = Order_details.Order_ID WHERE Orders.User_ID=:user_ID AND order_details.Status ="ACCEPT"');
+        $this->db->bind(':user_ID', $_SESSION['user_ID']);
+
+        return $this->db->resultSet();
+        
+    }
+
     public function getPendingOrders(){
         // $this->db->query('SELECT * FROM orders WHERE User_ID=:user_ID AND Status ="PENDING"'); 
         // $this->db->query('SELECT orders.*, order_details.*, item.Item_name FROM orders O INNER JOIN order_details D on O.Order_ID = D.Order_ID INNER JOIN item I on O.Item_ID = I.Item_Id WHERE O.User_ID=:user_ID AND D.Status ="PENDING"');
